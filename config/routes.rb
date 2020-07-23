611c65e357e4b4c70b91512f2fcaa2be08564653
Kurikichi <n79.katayama.yusuke@gmail.com>
2020-07-23 16:38:23 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :products
-  root 'items#index'
+  root 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
