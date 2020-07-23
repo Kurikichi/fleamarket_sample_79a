811e89a4ead650a44003b1fbcca98925cb7844d6
Kurikichi <n79.katayama.yusuke@gmail.com>
2020-07-23 18:38:28 +0900

    Update 20200723080442_create_images.rb

--- db/migrate/20200723080442_create_images.rb
+++ db/migrate/20200723080442_create_images.rb
@@ -1,8 +1,8 @@
 class CreateImages < ActiveRecord::Migration[6.0]
   def change
     create_table :images do |t|
-      t.integer :product_id
-      t.string :image
+      t.integer :product_id, null: false, foreign_key: true
+      t.string :image, null: false
 
       t.timestamps
     end
