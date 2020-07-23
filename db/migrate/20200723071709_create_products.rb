5f2bad8b8da5f80dc2e35dfb13c0029cfc5db819
Kurikichi <n79.katayama.yusuke@gmail.com>
2020-07-23 18:38:25 +0900

    Update 20200723071709_create_products.rb

--- db/migrate/20200723071709_create_products.rb
+++ db/migrate/20200723071709_create_products.rb
@@ -1,16 +1,16 @@
 class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
-      t.integer :category_id
-      t.integer :user_id
-      t.string :name
-      t.text :explanation
-      t.integer :price
+      t.integer :category_id, null:false, forign_key:true
+      t.integer :user_id, null:false, forign_key:true
+      t.string :name, null: false
+      t.text :explanation, null: false
+      t.integer :price, null: false
       t.string :brand
-      t.string :status
-      t.integer :shipping_charges
-      t.string :shipping_origin
-      t.integer :days_until_shipping
+      t.string :status, null: false
+      t.integer :shipping_charges, null: false
+      t.string :shipping_origin, null: false
+      t.integer :days_until_shipping, null: false
 
       t.timestamps
     end
