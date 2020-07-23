7a02f2624f5532cbf6bd3abda8d12e48ab5e22fd
Kurikichi <n79.katayama.yusuke@gmail.com>
2020-07-23 18:38:32 +0900

    Update 20200723090703_create_categories.rb

--- db/migrate/20200723090703_create_categories.rb
+++ db/migrate/20200723090703_create_categories.rb
@@ -1,8 +1,8 @@
 class CreateCategories < ActiveRecord::Migration[6.0]
   def change
     create_table :categories do |t|
-      t.string :category_name
-      t.string :ancestry
+      t.string :category_name, null: false
+      t.string :ancestry, null: false
 
       t.timestamps
     end
