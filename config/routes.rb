5a6f3c68926b4db908da0c1aedc60670232bd39f
satoshi9250 <67090591+satoshi9250@users.noreply.github.com>
2020-08-05 10:42:48 +0900

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,23 @@
 Rails.application.routes.draw do
 
-  devise_for :users
   root 'products#index'
   resources :products, except: :index
   resources :categories
   resources :images
-  resources :users, only: :show
  
  
+  resources :credit_cards, only: [:new, :create, :show, :destroy]
+
+  resources :users, only: [:index, :edit]
+  
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
+  devise_scope :user do
+    get 'delivery_address', to: 'users/registrations#new_delivery_address'
+    post 'delivery_address', to: 'users/registrations#create_delivery_address'
+  end
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
