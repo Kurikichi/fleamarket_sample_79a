52c54321607cb516069547c07bcb0ebd8c3b2e51
Kurikichi <n79.katayama.yusuke@gmail.com>
2020-08-06 10:58:12 +0900

    Update 20200804132537_add_references_to_products.rb

--- db/migrate/20200804132537_add_references_to_products.rb
+++ db/migrate/20200804132537_add_references_to_products.rb
@@ -1,5 +1,5 @@
 class AddReferencesToProducts < ActiveRecord::Migration[6.0]
   def change
-    # add_reference :products, :category, foreign_key: true
+    add_reference :products, :category, foreign_key: true
   end
 end
