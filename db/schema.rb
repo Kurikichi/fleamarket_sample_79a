cb8b8675e0739c626cd59062d8647234619bf7a3
satoshi9250 <67090591+satoshi9250@users.noreply.github.com>
2020-08-10 17:39:47 +0900

    edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_08_06_051457) do
 
-
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "ancestry", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "ancestry"
     t.string "name"
-    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -69,16 +66,11 @@ ActiveRecord::Schema.define(version: 2020_08_06_051457) do
     t.integer "shipping_charges", null: false
     t.integer "shipping_origin", null: false
     t.integer "days_until_shipping", null: false
-    t.bigint "seller_id"
-    t.bigint "buyer_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "exhibition_status"
     t.bigint "category_id"
     t.index ["category_id"], name: "index_products_on_category_id"
-
-    t.index ["seller_id"], name: "index_products_on_seller_id"
-
   end
 
   create_table "purchases", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -109,8 +101,4 @@ ActiveRecord::Schema.define(version: 2020_08_06_051457) do
   add_foreign_key "credit_cards", "users"
   add_foreign_key "images", "products"
   add_foreign_key "products", "categories"
-
-  add_foreign_key "products", "users", column: "buyer_id"
-  add_foreign_key "products", "users", column: "seller_id"
-
 end
